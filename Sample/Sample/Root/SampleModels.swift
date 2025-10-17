2b408020821e4ac1f7b4f947dcd5dd04f86c9e1a
OhKanghoon <ggaa96@naver.com>
2025-10-17 14:00:33 +0900

    sample: add flexlayout sample

--- Sample/Sample/Root/SampleModels.swift
+++ Sample/Sample/Root/SampleModels.swift
@@ -16,10 +16,12 @@ protocol SampleViewModel {
 
 enum SampleRow: Int, CaseIterable {
   case plain
+  case flex
 
   var viewModel: SampleViewModel {
     switch self {
     case .plain: return PlainViewModel()
+    case .flex: return FlexViewModel()
     }
   }
 }
@@ -29,6 +31,11 @@ extension SampleRow {
   struct PlainViewModel: SampleViewModel {
     let title = "Plain"
     let rowViewController: FloatingBottomSheet = PlainViewController()
+  }
+
+  struct FlexViewModel: SampleViewModel {
+    let title = "Flex"
+    let rowViewController: FloatingBottomSheet = FlexViewController()
   }
 }
 
---
+++ Sample/Sample/ViewControllers/FlexViewController.swift
@@ -0,0 +1,95 @@
+//
+//  FlexViewController.swift
+//  Sample
+//
+//  Created by Ray on 10/17/25.
+//
+
+import UIKit
+
+import FlexLayout
+import FloatingBottomSheet
+import PinLayout
+
+final class FlexContentView: UIView {
+
+  private let rootFlexContainer = UIView()
+
+  private let label: UILabel = {
+    let label = UILabel()
+    label.text = "This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view.This is a FlexLayout-based content view."
+    label.textAlignment = .center
+    label.numberOfLines = 0
+    return label
+  }()
+
+  private let ctaButton: UIButton = {
+    let button = UIButton()
+    button.configuration = .filled()
+    button.setTitle("Click Me", for: .normal)
+    return button
+  }()
+
+  init() {
+    super.init(frame: .zero)
+    defineLayout()
+    backgroundColor = .systemBackground
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func defineLayout() {
+    addSubview(rootFlexContainer)
+
+    rootFlexContainer.flex.direction(.column).padding(16).define { flex in
+      flex.addItem(label).marginBottom(12)
+      flex.addItem(ctaButton).height(44)
+    }
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    rootFlexContainer.pin.all(pin.safeArea)
+    rootFlexContainer.flex.layout()
+  }
+
+  override func sizeThatFits(_ size: CGSize) -> CGSize {
+    let result = rootFlexContainer.flex.sizeThatFits(
+      size: .init(
+        width: size.width,
+        height: .nan,
+      ),
+    )
+
+    print(result)
+
+    return result
+  }
+}
+
+final class FlexViewController: UIViewController {
+
+  lazy var contentView = FlexContentView()
+
+  override func loadView() {
+    view = contentView
+  }
+}
+
+extension FlexViewController: FloatingBottomSheetPresentable {
+  var bottomSheetScrollable: UIScrollView? {
+    nil
+  }
+
+  var bottomSheetHeight: CGFloat {
+    contentView.sizeThatFits(
+      CGSize(
+        width: view.frame.width,
+        height: .greatestFiniteMagnitude,
+      ),
+    ).height
+  }
+}
